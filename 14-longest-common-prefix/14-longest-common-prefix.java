f5d6f83c161214f0f21991a9f9af597fa820bfd3
Alexey Zausalin <8299223+alexey-zausalin@users.noreply.github.com>
2022-03-20 18:10:21 +0200

    Time: 1 ms (82.58%), Space: 42 MB (49.74%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
+        int minLength = minLength(strs);
+        for (int i = 0; i < minLength; i++) {
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j-1].charAt(i) != strs[j].charAt(i)) {
+                    return strs[j-1].substring(0, i);
+                }
+            }
+        }
+
+        return strs[0].substring(0, minLength);
+    }
+
+    private int minLength(String[] strs) {
+        if (strs.length == 0) {
+            return 0;
+        }
+
+        int minLength = strs[0].length();
+        for (String str: strs) {
+            if (minLength > str.length()) {
+                minLength = str.length();
+            }
+        }
+
+        return minLength;
+    }
+}
