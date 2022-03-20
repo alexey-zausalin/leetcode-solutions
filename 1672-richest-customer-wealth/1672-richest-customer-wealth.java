bce8876adeb393a41d2e0e9b25bbcf2867616c92
Alexey Zausalin <8299223+alexey-zausalin@users.noreply.github.com>
2022-03-20 17:38:17 +0200

    Time: 0 ms (100.00%), Space: 43.9 MB (13.68%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,7 +1,22 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-        return Arrays.stream(accounts)
-            .mapToInt(account -> Arrays.stream(account).sum())
-            .max().orElse(0);
+        int maxWealth = 0;
+        for (int[] wealth: accounts) {
+            int currentWealth = totalWealth(wealth);
+            if (currentWealth > maxWealth) {
+                maxWealth = currentWealth;
+            }
+        }
+
+        return maxWealth;
+    }
+
+    private int totalWealth(int[] wealth) {
+        int total = 0;
+        for (int currentWealth: wealth) {
+            total += currentWealth;
+        }
+
+        return total;
     }
 }
