ea2b25d94a5281630a09e8f8f63af5cd2bacbf56
Alexey Zausalin <8299223+alexey-zausalin@users.noreply.github.com>
2022-03-28 16:18:52 +0300

    Time: 1 ms (58.40%), Space: 41.7 MB (10.01%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,5 +1,19 @@
 class Solution {
     public int numberOfSteps(int num) {
+        if (num == 0) {
+            return 0;
+        }
+
+        int steps = 0;
+        while (num > 0) {
+            steps += (num & 1) + 1;
+            num = num >> 1;
+        }
+        
+        return steps - 1;
+    }
+    
+    public int numberOfStepsTrivial(int num) {
         int steps = 0;
         for (;num > 0; steps++) {
             if (num % 2 != 0) {
