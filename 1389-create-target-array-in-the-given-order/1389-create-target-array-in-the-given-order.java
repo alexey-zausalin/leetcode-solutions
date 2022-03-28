39c88e8d5fbbfd65392d227f3db3b6f36647d2dd
Alexey Zausalin <8299223+alexey-zausalin@users.noreply.github.com>
2022-03-28 17:53:32 +0300

    Time: 1 ms (76.23%), Space: 42.3 MB (60.04%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.java
@@ -1,5 +1,19 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> listNums = new ArrayList();
+        for (int i = 0; i < nums.length; i++) {
+            listNums.add(index[i], nums[i]);
+        }
+        
+        int[] target = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            target[i] = listNums.get(i);
+        }
+        
+        return target;
+    }
+    
+    public int[] createTargetArrayDoubleLoops(int[] nums, int[] index) {
         int[] target = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
             int curIndex = index[i];
