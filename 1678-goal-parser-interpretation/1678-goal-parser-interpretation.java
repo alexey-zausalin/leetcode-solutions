fc7cefc80d4459b6d0293ba875e50a282d632d0d
Alexey Zausalin <8299223+alexey-zausalin@users.noreply.github.com>
2022-03-29 11:28:55 +0300

    Time: 0 ms (100.00%), Space: 42.8 MB (10.33%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,5 +1,25 @@
 class Solution {
     public String interpret(String command) {
+        StringBuilder interpretedCommand = new StringBuilder();
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == '(') {
+                if (command.charAt(i+1) == ')') {
+                    interpretedCommand.append("o");
+                    i += 1;
+                } else {
+                    interpretedCommand.append("al");
+                    i += 3;
+                }
+            } else {
+                interpretedCommand.append("G");
+            }
+        }
+
+        return interpretedCommand.toString();
+    }
+    
+    // With two string builders
+    public String interpretWithTwoStringBuilder(String command) {
         Map<String, String> alphabet = Map.of(
             "G", "G",
             "()", "o",
