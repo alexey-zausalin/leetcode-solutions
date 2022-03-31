59b5702cb7a2f99fa4fec6971ee7aa428a8a1e40
Alexey Zausalin <8299223+alexey-zausalin@users.noreply.github.com>
2022-03-31 22:50:54 +0300

    Time: 6 ms (30.46%), Space: 43.6 MB (70.52%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         Set<Integer> set = (nums1.length < nums2.length) ?
-            getIntersection(nums1, nums2) :
-            getIntersection(nums2, nums1);
+            innerIntersection(nums1, nums2) :
+            innerIntersection(nums2, nums1);
             
         int key = 0;
         int[] intersection = new int[set.size()];
@@ -13,7 +13,7 @@ class Solution {
         return intersection;
     }
     
-    private Set<Integer> getIntersection(int[] inNums, int[] toNums) {      
+    private Set<Integer> innerIntersection(int[] inNums, int[] toNums) {      
         Set<Integer> set = new HashSet();
         
         Arrays.sort(inNums);
@@ -35,30 +35,6 @@ class Solution {
                 }
 
                 if (toNums[med] < inNums[i]) {
-                    lo = med + 1;
-                } else {
-                    hi = med - 1;
-                }
-            }
-        }
-
-        return set;
-    }
-    
-    private Set<Integer> getIntersectionV2(int[] inNums, int[] toNums) {
-        Set<Integer> set = new HashSet();
-        
-        Arrays.sort(toNums);
-        for (int inNum: inNums) {
-            int lo = 0, hi = toNums.length - 1;
-            while (lo <= hi) {
-                int med = lo + (hi - lo) / 2;
-                if (toNums[med] == inNum) {
-                    set.add(inNum);
-                    break;
-                }
-
-                if (toNums[med] < inNum) {
                     lo = med + 1;
                 } else {
                     hi = med - 1;
