387a769b5dfe8e8e9d4b14a1ac9f426cda527900
Alexey Zausalin <8299223+alexey-zausalin@users.noreply.github.com>
2022-04-02 23:43:42 +0300

    Time: 10 ms (60.02%), Space: 54.7 MB (41.58%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean validPalindrome(String s) {
         int lo = 0, hi = s.length() - 1;
-        while (lo <= hi) {
+        while (lo < hi) {
             if (s.charAt(lo) != s.charAt(hi)) {
                 return isAbsolutePalindrome(s.substring(lo, hi)) || isAbsolutePalindrome(s.substring(lo+1, hi+1));
             }
@@ -15,7 +15,7 @@ class Solution {
     
     private boolean isAbsolutePalindrome(String s) {
         int lo = 0, hi = s.length() - 1;
-        while (lo <= hi) {
+        while (lo < hi) {
             if (s.charAt(lo++) != s.charAt(hi--)) {
                 return false;
             }
