e8cf9e76989a9e5c7bf56d860d4572ca19334d23
Alexey Zausalin <8299223+alexey-zausalin@users.noreply.github.com>
2022-04-06 12:49:19 +0300

    Time: 2 ms (58.30%), Space: 41.9 MB (41.00%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -1,5 +1,31 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
+        Arrays.sort(nums);
+
+        int sum = 0;
+        int lastNum = 0, count = 0;
+        for (Integer num: nums) {
+            if (num == lastNum) {
+                count++;
+                continue;
+            }
+
+            if (count == 1) {
+                sum += lastNum;
+            }
+
+            lastNum = num;
+            count = 1;
+        }
+
+        if (count == 1) {
+            sum += lastNum;
+        }
+        
+        return sum;
+    }
+    
+    public int sumOfUniqueWithHash(int[] nums) {
         Map<Integer, Integer> map = new HashMap();
         for (Integer num: nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
