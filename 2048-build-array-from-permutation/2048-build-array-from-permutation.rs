f327a1070d354abe10b49c81bcabe6009de1c034
“a.zausalin” <alexeyzausalin@gmail.com>
2023-06-04 12:20:24 +0300

    Add tests for "Build Array from Permutation" rust solution

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.rs
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.rs
@@ -1,5 +1,28 @@
+struct Solution;
+
 impl Solution {
     pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
         nums.iter().map(|num| nums[*num as usize]).collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(
+            Solution::build_array(vec![0, 2, 1, 5, 3, 4]),
+            vec![0, 1, 2, 4, 5, 3]
+        );
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(
+            Solution::build_array(vec![5, 0, 1, 2, 3, 4]),
+            vec![4, 5, 0, 1, 2, 3]
+        );
     }
 }
