88e0e4eeca28b0d33ad21aa6ce6b216f983a9e36
“a.zausalin” <alexeyzausalin@gmail.com>
2023-06-04 12:49:18 +0300

    Add tests for "Concatenation of Array" rust solution

--- 2058-concatenation-of-array/2058-concatenation-of-array.rs
+++ 2058-concatenation-of-array/2058-concatenation-of-array.rs
@@ -1,7 +1,30 @@
+struct Solution;
+
 impl Solution {
     pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
         let mut result = nums.clone();
         result.extend(nums);
         result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(
+            Solution::get_concatenation(vec![1, 2, 1]),
+            vec![1, 2, 1, 1, 2, 1]
+        );
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(
+            Solution::get_concatenation(vec![1, 3, 2, 1]),
+            vec![1, 3, 2, 1, 1, 3, 2, 1]
+        );
     }
 }
