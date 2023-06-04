8461f07cd93822a680f6d0651849094e58d70eed
“a.zausalin” <alexeyzausalin@gmail.com>
2023-06-04 12:09:07 +0300

    Add tests for "Two sum" rust solution

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 
+struct Solution;
+
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut bufer: HashMap<i32, i32> = HashMap::new();
@@ -10,5 +12,31 @@ impl Solution {
             bufer.insert(num, i as i32);
         }
         vec![]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = vec![2, 7, 11, 15];
+
+        assert_eq!(Solution::two_sum(input, 9), vec![0, 1]);
+    }
+
+    #[test]
+    fn test_2() {
+        let input = vec![3, 2, 4];
+
+        assert_eq!(Solution::two_sum(input, 6), vec![1, 2]);
+    }
+
+    #[test]
+    fn test_3() {
+        let input = vec![3, 3];
+
+        assert_eq!(Solution::two_sum(input, 6), vec![0, 1]);
     }
 }
