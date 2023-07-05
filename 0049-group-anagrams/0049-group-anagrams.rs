332c457ca1130d4e78fbb5f4637533e8bc0c4b34
Oleksiy Zausalin <oleksiy.zausalin@yalantis.net>
2023-07-05 19:31:19 +0300

    Add tests for "Group Anagrams" rust solution

--- 0049-group-anagrams/0049-group-anagrams.rs
+++ 0049-group-anagrams/0049-group-anagrams.rs
@@ -22,3 +22,35 @@ impl Solution {
         buffer.into_values().collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let input = vec!["eat", "tea", "tan", "ate", "nat", "bat"]
+            .into_iter()
+            .map(String::from)
+            .collect();
+        let output = vec![vec!["bat"], vec!["nat", "tan"], vec!["ate", "eat", "tea"]];
+
+        assert_eq!(Solution::group_anagrams(input), output);
+    }
+
+    #[test]
+    fn test_2() {
+        let input = vec![""].into_iter().map(String::from).collect();
+        let output = vec![vec![""]];
+
+        assert_eq!(Solution::group_anagrams(input), output);
+    }
+
+    #[test]
+    fn test_3() {
+        let input = vec!["a"].into_iter().map(String::from).collect();
+        let output = vec![vec!["a"]];
+
+        assert_eq!(Solution::group_anagrams(input), output);
+    }
+}
